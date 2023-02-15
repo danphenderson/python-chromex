8045900951075a543bf8de86bf2bad028e9488b5
danphenderson <danphenderson@gmail.com>
2023-02-15 16:56:11 -0700

    feat : HRef inherets from type 'AsyncBaseModel'

--- chromex/href.py
+++ chromex/href.py
@@ -1,57 +1,35 @@
-from pydantic import BaseModel, Field, AnyHttpUrl, root_validator, UUID5
+from pydantic import Field, AnyHttpUrl, root_validator, UUID5
 from uuid import uuid5, NAMESPACE_URL
-from asyncio import get_event_loop, ensure_future, Future, gather
+from asyncio import Future
+from chromex.abstract import AsyncBaseModel
 
-class HRef(BaseModel):
+class HRef(AsyncBaseModel):
     url: AnyHttpUrl
-    is_static: bool = Field(default=False)
     id: UUID5 | None = Field(default=None) # sha1 hash of url for unique id
-    _tasks: list[Future] = Field(default_factory=list)
-
-    @staticmethod
-    async def _run_sync(func):
-        loop = get_event_loop()
-        return await loop.run_in_executor(None, func)
     
     @classmethod
-    async def from_url(cls, url, *args, **kwargs) -> "HRef":
+    async def from_url(cls, url, *args, **kwargs) -> Future["HRef"]:
         async with cls(url=url, *args, **kwargs) as href:
-            return href
-
-    async def run_async(self, func):
-        task = ensure_future(self._run_sync(func))
-        self._tasks.append(task)
-        return await task
+            return href # type: ignore
 
     @root_validator
-    def assign_id(cls, values):
+    def assign_id(cls, values: dict) -> dict:
         values["id"] = uuid5(NAMESPACE_URL, values["url"])
         return values
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if isinstance(other, str):
             return self.url == other
         elif isinstance(other, HRef):
             return self.url == other.url
         return False
 
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
         return self.url != other.url
 
     def __str__(self) -> str:
         return str(self.url)
 
-    async def __aenter__(self):
-        return self
-
     async def __hash__(self):
         return hash(self.id)
-
-    async def __await__(self):
-        return self._run_sync(lambda: self).__await__()
-
-    async def __aexit__(self, exc_type, exc_value, traceback):
         
-        if self._tasks:
-            await gather(*self._tasks)
-        return None
